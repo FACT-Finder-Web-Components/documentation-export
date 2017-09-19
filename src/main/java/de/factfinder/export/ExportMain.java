45644ddb85fbab0ff419c7d6db8a5a4f70399000
Sebastian Damrath <sebastian.damrath@omikron.net>
2017-09-19 18:30:44 +0200

    reading md files recursively from directory

--- src/main/java/de/factfinder/export/ExportMain.java
+++ src/main/java/de/factfinder/export/ExportMain.java
@@ -1,7 +1,20 @@
 package de.factfinder.export;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+import de.factfinder.export.io.FileWalker;
+
 public class ExportMain {
 
-	public static void main(final String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		List<File> files = FileWalker.readMarkdownFilesRecursively(args[0]);
+		File file = files.get(0);
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		br.lines().forEach(System.out::println);
 	}
 }
---
+++ src/main/java/de/factfinder/export/io/FileWalker.java
@@ -0,0 +1,34 @@
+package de.factfinder.export.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Provides functionality to traverse directories and find markdown files easily
+ */
+public final class FileWalker {
+
+	private FileWalker() {
+		//util
+	}
+
+	public static List<File> readMarkdownFilesRecursively(String baseDir) {
+		List<File> markdownFiles = null;
+		try {
+			markdownFiles = Files.walk(Paths.get(baseDir))
+								 .filter(Files::isRegularFile)
+								 .map(Path::toFile)
+								 .filter(File::canRead)
+								 .filter(file -> file.getName().endsWith("md"))
+								 .collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return markdownFiles;
+	}
+}
